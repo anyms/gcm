395ccc20c36dc635bb983ca61930b8164c3a16f9
anyms <mytellee@gmail.com>
2021-05-26 03:44:20 +0530

    :sparkles: updating

--- gcm.py
+++ gcm.py
@@ -8,19 +8,25 @@ class GCM:
     def __init__(self):
         parser = ArgumentParser(description="Github commit management tool")
         parser.add_argument("cmd", type=str, help="command to exucute")
+        parser.add_argument("url", type=str, help="Github repo URL", nargs="?")
         self.args = parser.parse_args()
 
     def run(self):
         branch = os.popen("git branch").read().replace("*", "").strip()
         if self.args.cmd == "branch":
-            pass
+            os.system("git branch")
+        elif self.args.cmd == "clone":
+            if self.args.url is None:
+                print("[Error] Repo URL required to clone")
+                return
+            os.system("git clone {}".format(self.args.url))
         else:
             print("adding files...")
-            os.system("git add .")
+            os.popen("git add .").read()
             print("commiting files...")
-            os.system('git commit -m "{}"'.format(self.args.cmd.replace('"', '\\"')))
+            os.popen('git commit -m "{}"'.format(self.args.cmd.replace('"', '\\"'))).read()
             print("pushing files...")
-            os.system("git push origin {}".format(branch))
+            os.popen("git push origin {}".format(branch)).read()
             print("done.")
 
 
